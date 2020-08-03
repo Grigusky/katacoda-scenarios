d0b53288b2e3c2a0ab9e4b3854b38dbc1f5cf8a9
Grégoire MEUNIER <x202434_na@eur.msd.world.socgen>
2020-08-03 17:18:31 +0200

    push showide

--- sg_watch_dog/env-init.sh
+++ sg_watch_dog/env-init.sh
@@ -1,9 +1,5 @@
 apt-get install python 3.6
 alias python3=/usr/bin/python3.6
-mkdir docker_project
-touch main.py
-touch docker_project/main.py
 pip3 install virtualenv
 python3.6 -m virtualenv .venv
 source .venv/bin/activate
-pip install -r requirments.txt
