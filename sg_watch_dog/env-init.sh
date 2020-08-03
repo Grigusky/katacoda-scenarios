3ea466534100482062eb59c4aaa578896bdba7bd
Grégoire MEUNIER <x202434_na@eur.msd.world.socgen>
2020-08-03 17:55:39 +0200

    push showide

--- sg_watch_dog/env-init.sh
+++ sg_watch_dog/env-init.sh
@@ -1,5 +1,3 @@
-apt-get install python 3.6
-alias python3=/usr/bin/python3.6
-pip3 install virtualenv
-python3.6 -m virtualenv .venv
-source .venv/bin/activate
+virtualenv /root/test/files/.venv
+source /root/test/files/.venv/bin/activate
+pip install -r /root/test/files/requirements.txt
