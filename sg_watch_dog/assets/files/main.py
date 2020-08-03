fd58a398f8509e451f07ac75ac0b1d443b526a66
Grégoire MEUNIER <x202434_na@eur.msd.world.socgen>
2020-08-03 17:34:37 +0200

    push showide

--- sg_watch_dog/assets/files/main.py
+++ sg_watch_dog/assets/files/main.py
@@ -1,1 +1,13 @@
-# oukai
+#coding: utf-8
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+from werkzeug.contrib.fixers import ProxyFix
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='1.0', title='TodoMVC API',
+    description='A simple TodoMVC API',
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
